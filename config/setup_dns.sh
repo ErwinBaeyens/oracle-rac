5fee9bcb9254b15796e2f4198c029b9da667ee29
Erwin Baeyens <Erwin.Baeyens@ypto.be>
2019-02-13 15:30:06 +0100

    corrected shell scripts, they need +x

--- config/setup_dns.sh
+++ config/setup_dns.sh
@@ -11,21 +11,13 @@ reset=$(tput sgr0)
 echo "192.168.10.20 racknode-1" | tee -a /etc/hosts
 echo "192.168.10.30 racknode-2" | tee -a /etc/hosts
 
-
-
-
-sudo timedatectl set-timezone Europe/Brussels
-sudo systemctl start ntpd
-sudo firewall-cmd --reload
-
-
-
-
 # installation of needed packages
 echo "${bold}${green}installation of needed packages${reset}"
 yum groupinstall -y "Base"
 yum groupinstall -y "Core"
 yum install -y ntp bind bind-utils  
+sudo timedatectl set-timezone Europe/Brussels
+sudo systemctl start ntpd
 
 # firewall settings
 echo "${bold}${green}firewall settings${reset}"
@@ -35,7 +27,7 @@ firewall-cmd --permanent --add-service=ntp
 sudo firewall-cmd --permanent --zone=public --add-port=53/udp
 sudo firewall-cmd --permanent --zone=public --add-port=53/udp
 
-
+sudo firewall-cmd --reload
 # firewall-cmd --permanent --zone=public --add-rich-rule 'rule family="ipv4" source address="10.0.0.0/8" port port=5500 protocol=tcp accept'
 
 # firewall-cmd --permanent --zone=public --add-rich-rule 'rule family="ipv4" source address="10.0.0.0/8" port port="1521" protocol="tcp" accept'
