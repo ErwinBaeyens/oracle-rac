8ece2d1abf2bfc197a135891dbab1b65de21e523
Erwin Baeyens <erwin@m4800.piwi.home>
2019-02-13 13:17:09 +0100

    added echo statements to the provision script

--- config/setup_db_node.sh
+++ config/setup_db_node.sh
@@ -4,6 +4,8 @@ set -e
 sudo echo "192.168.10.20 racknode-1" | sudo tee -a /etc/hosts
 sudo echo "192.168.10.30 racknode-2" | sudo tee -a /etc/hosts
 
+# installation of needed packages
+echo "installation of needed packages"
 sudo yum groupinstall -y "Base"
 sudo yum groupinstall -y "Core"
 sudo yum install -y ntp binutils libX11 compat-libcap1 libXau cpmpat-libstdc++33 libaio libaio-devel gcc libdmx
@@ -11,6 +13,7 @@ sudo yum install -y glibc-devel glibc ksh make libgcc sysstat libstdc++ xorg-x11
 sudo yum install -y libXtst libXi libxcb libXt libXmu libXxf86misc libXxf86dga LibXxf86vm nfs-utils
 
 # firewall settings
+echo "firewall settings"
 sudo systemctl enable firewalld
 sudo systemctl start firewalld
 sudo firewall-cmd --permanent --add-serviysctlce=ntp
@@ -18,11 +21,13 @@ sudo firewall-cmd --permanent --zone=public --add-rich-rule="rule family="ipv4"s
 sudo firewall-cmd --permanent --zone=public --add-rich-rule="rule family="ipv4"source address=10.0.0.0/8" port protocol="tcp" port="5500 " accept"
 
 # enable and start ntpd
+echo " enable and start ntpd"
 sudo timedatectl set-timezone Europe/Brussels
 sudo systemctl start ntpd
 sudo firewall-cmd --reload
 
 # stop and disable avahi-daemon service
+echo"stop and disable avahi-daemon service"
 sudo systemctl stop avahi-dnsconfd
 sudo systemctl stop avahi-daemon
 
@@ -34,6 +39,7 @@ rm '/etc/systemd/system/multi-user.target.wants/avahi-daemon.service'
 rm '/etc/systemd/system/sockets.target.wants/avahi-daemon.socket'
 
 # create the needed accounts and groups
+echo "create the needed accounts and groups"
 groupadd --gid 54321 oinstall
 groupadd --gid 54322 dba
 groupadd --gid 54323 asmdba
@@ -45,7 +51,7 @@ groupadd --gid 54327 backupdba
 groupadd --gid 54328 dgdba
 groupadd --gid 54329 kmdba
 
-# create the neede users
+# create the needed users
 useradd --uid 54321 --gid oinstall --groups dba,oper,asmdba,asmoper,backupdba,dgdba,kmdba
 oracle
 : oswd oracle
